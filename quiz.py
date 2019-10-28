cce60c7b3dd43552a512704fa16a74718f5b1b36
maxmahl <maxalexander@hotmail.nl>
2019-10-28 15:42:36 +0100

    Edit

--- quiz.py
+++ quiz.py
@@ -1,2 +1,6 @@
-print("Welkom bij de Marvel Quizz!")
-weqw
+import requests
+import hashlib
+import time
+import json
+public='05e07ce2914b79046f157b3f7eee36b3'
+private='c1e103dcdbd9d9c0bec980663077e52b346317e2'
