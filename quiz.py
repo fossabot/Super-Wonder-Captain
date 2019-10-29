f5a42d6645e42997c6db291228b9c12cc8e3e577
maxmahl <maxalexander@hotmail.nl>
2019-10-29 08:57:58 +0100

    GUI

--- quiz.py
+++ quiz.py
@@ -6,7 +6,6 @@ import json
 public='05e07ce2914b79046f157b3f7eee36b3'
 private='c1e103dcdbd9d9c0bec980663077e52b346317e2'
 
-
 class Window(Frame):
     def __init__(self, master=None):
         Frame.__init__(self, master)
