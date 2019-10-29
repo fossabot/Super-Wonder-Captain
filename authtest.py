fb10ae3cd492a4efa0cb04236bdb4f6a17473fc1
Ruben <15885455+RubenNL@users.noreply.github.com>
2019-10-29 08:51:44 +0100

    login info in eigen configuratiebestand

--- authtest.py
+++ authtest.py
@@ -2,12 +2,13 @@ import requests
 import hashlib
 import time
 import json
-public='05e07ce2914b79046f157b3f7eee36b3'
-private='c1e103dcdbd9d9c0bec980663077e52b346317e2'
+loginInfo=json.load(open('apikey.json','r'))
+private=loginInfo['privatekey']
+public=loginInfo['pubkey']
 def sendRequest(request):
 	stamp=str(time.time())
-	part=stamp+private+public
-	hash=hashlib.md5(part.encode()).hexdigest()
+	hashString=stamp+private+public
+	hash=hashlib.md5(hashString.encode()).hexdigest()
 	httprequest=requests.get(f'http://gateway.marvel.com/v1/public/{request}?ts={stamp}&apikey={public}&hash={hash}')
-	return json.loads(httprequest.text)['data']
-print(sendRequest('comics'))
+	return json.loads(httprequest.text)['data']['results']
+print(json.dumps(sendRequest('characters'), sort_keys=True,indent=4, separators=(',', ': ')))
