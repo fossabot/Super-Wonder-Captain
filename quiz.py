8534883e5f4d52540b5e9d08b046161d6bdcac18
Ruben <15885455+RubenNL@users.noreply.github.com>
2019-10-29 09:56:58 +0100

    select names, select name, description

--- quiz.py
+++ quiz.py
@@ -3,16 +3,39 @@ import hashlib
 import time
 from tkinter import *
 import json
+import random
 
 def sendMarvelRequest(request):
+	'stuurt een aanvraag naar de Marvel API'
 	loginInfo=json.load(open('apikey.json','r'))
 	stamp=str(time.time())
 	pubkey=loginInfo['pubkey']
 	privatekey=loginInfo['privatekey']
 	hashString=stamp+privatekey+pubkey
 	hash=hashlib.md5(hashString.encode()).hexdigest()
-	httprequest=requests.get(f'http://gateway.marvel.com/v1/public/{request}?ts={stamp}&apikey={pubkey}&hash={hash}')
+	httprequest=requests.get(f'https://gateway.marvel.com/v1/public/{request}&ts={stamp}&apikey={pubkey}&hash={hash}')
 	return json.loads(httprequest.text)['data']['results']
-#print(sendMarvelRequest('characters'))
 
-print(sendMarvelRequest('characters'))
+def selectCharacter():
+	while True:
+		randomNumber=random.randint(0,1400)
+		characters=sendMarvelRequest(f'characters?offset={randomNumber}')
+		for character in characters:
+			if len(character['description'])>0:
+				return character,characters
+def selectNames(characters,exclude):
+	names=[exclude]
+	while len(names)<4:
+		character=random.choice(characters)
+		if character['name'] not in names:
+			names.append(character['name'])
+	return names
+		
+#print(json.dumps(sendMarvelRequest('characters'), sort_keys=True,indent=4, separators=(',', ': ')))
+character,characters=selectCharacter()
+name=character['name']
+names=selectNames(characters,name)
+description=character['description'].replace(name,'<naam>')
+print(names)
+print(description)
+print(name)
